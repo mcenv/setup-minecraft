8ac64f4dae4780e70b8330985f5d68a4fc8b2752
intsuc <intsuc@gmail.com>
2021-06-24 16:24:13 +0900

    Use @actions packages

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const VERSION_MANIFEST_V2_URL = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
 export const MINECRAFT = "minecraft";
+export const SERVER = "server.jar";
+export const SCRIPT = "minecraft";
 
 export const INPUT_VERSION = "version";
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { getInput, info, setFailed, setOutput } from "@actions/core";
-import { restoreCache, saveCache } from "@actions/cache";
+import * as cache from "@actions/cache";
+import * as core from "@actions/core";
+import { HttpClient } from "@actions/http-client";
+import * as io from "@actions/io";
+import * as tc from "@actions/tool-cache";
 import * as fs from "fs";
-import * as https from "https";
 import * as path from "path";
-import { INPUT_VERSION, MINECRAFT, OUTPUT_VERSION, VERSION_MANIFEST_V2_URL } from "./constants";
+import { INPUT_VERSION, MINECRAFT, OUTPUT_VERSION, SCRIPT, SERVER, VERSION_MANIFEST_V2_URL } from "./constants";
 
 interface VersionManifestV2 {
     latest: {
@@ -33,64 +35,56 @@ interface Version {
     }
 }
 
-async function getJson<T>(url: string): Promise<T> {
-    return new Promise(resolve => https.get(url, res => {
-        let data = "";
-        res
-            .on("data", chunk => data += chunk)
-            .on("end", () => resolve(JSON.parse(data)));
-    }));
-}
-
-async function downloadServer(url: string): Promise<void> {
-    info(`Downloading server.jar from ${url} ...`);
-
-    const file = path.join(MINECRAFT, "server.jar");
-    return new Promise((resolve, reject) => https.get(url, res => {
-        res
-            .pipe(fs.createWriteStream(file))
-            .on("finish", () => resolve())
-            .on("error", error => reject(error));
-    }));
+async function getJson<T>(http: HttpClient, url: string): Promise<T> {
+    const response = await http.get(url);
+    const body = await response.readBody();
+    return JSON.parse(body);
 }
 
 async function run(): Promise<void> {
     try {
-        fs.mkdirSync(MINECRAFT, { recursive: true });
+        const http = new HttpClient();
 
-        const versionManifest = await getJson<VersionManifestV2>(VERSION_MANIFEST_V2_URL);
+        const versionManifest = await getJson<VersionManifestV2>(http, VERSION_MANIFEST_V2_URL);
 
-        let version = getInput(INPUT_VERSION);
-        switch (version) {
-            case "release":
-                version = versionManifest.latest.release;
-                break;
-            case "snapshot":
-                version = versionManifest.latest.snapshot;
-                break;
-        }
+        const version = (() => {
+            const version = core.getInput(INPUT_VERSION);
+            switch (version) {
+                case "release":
+                    return versionManifest.latest.release;
+                case "snapshot":
+                    return versionManifest.latest.snapshot;
+                default:
+                    return version;
+            }
+        })();
 
         const versionEntry = versionManifest.versions.find(v => v.id === version);
         if (!versionEntry) {
-            throw Error(`Version ${version} not found`);
+            throw Error(`Version '${version}' not found`);
         }
 
-        const key = `${MINECRAFT}-${versionEntry.id}`;
+        await io.mkdirP(MINECRAFT);
+
+        const key = `${MINECRAFT}-${version}`;
         const paths = [MINECRAFT];
-        const cacheKey = await restoreCache(paths, key);
+        const cacheKey = await cache.restoreCache(paths, key);
         if (!cacheKey) {
-            const targetVersion = await getJson<Version>(versionEntry.url);
-            await downloadServer(targetVersion.downloads.server.url);
-            await saveCache(paths, key);
+            const targetVersion = await getJson<Version>(http, versionEntry.url);
+            await tc.downloadTool(targetVersion.downloads.server.url, path.join(MINECRAFT, SERVER));
+
+            fs.writeFileSync(path.join(MINECRAFT, SCRIPT), `java -jar $(dirname $(realpath $0))/${SERVER} nogui`);
+
+            await cache.saveCache(paths, key);
         }
 
-        setOutput(OUTPUT_VERSION, version);
+        core.addPath(MINECRAFT);
+        core.setOutput(OUTPUT_VERSION, version);
 
-        info("Minecraft:");
-        info(`  Version: ${version}`);
-        info(`  Path: ${MINECRAFT}`);
+        core.info("Minecraft:");
+        core.info(`  Version: ${version}`);
     } catch (error) {
-        setFailed(error.message);
+        core.setFailed(error.message);
     }
 }
 
