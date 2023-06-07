48e0635d62e4317ff95154e8d59637d581444031
intsuc <intsuc@gmail.com>
2023-06-07 11:55:46 +0900

    Resolve

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { mkdirP } from "@actions/io";
 import { downloadTool } from "@actions/tool-cache";
 import { createHash } from "crypto";
 import { stat, readFile } from "fs/promises";
-import { join } from "path";
+import { join, resolve } from "path";
 
 /**
  * @typedef {{
@@ -43,8 +43,8 @@ import { join } from "path";
 
 const VERSION_MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
 const CACHE_KEY_PREFIX = "minecraft";
-const ROOT_PATH = process.platform === "win32" ? "C:\\minecraft" : "/minecraft";
-const SERVER_JAR_PATH = join(ROOT_PATH, "server.jar");
+const ROOT_PATH = ".minecraft";
+const SERVER_JAR_PATH = resolve(ROOT_PATH, "server.jar");
 const SERVER_JAR_ENV = "MINECRAFT";
 const INPUT_VERSION = "version";
 const OUTPUT_VERSION = "version";
